957341e3bb29c9114ab49732a395f9104c75e46c
Harrison McCullough <mccullough.harrison@gmail.com>
2019-08-04 19:42:01 -0600

    Implement add/remove row/column messages and buttons

--- src/Main.elm
+++ src/Main.elm
@@ -59,6 +59,8 @@ type Msg
     = NewColor Int Int Color
     | AddRow
     | AddColumn
+    | RemoveRow
+    | RemoveColumn
 
 update : Msg -> Model -> Model
 update msg model =
@@ -75,6 +77,14 @@ update msg model =
           Array2D.appendColumn Array.empty defaultColor model.colors
               |> asColorsIn model
 
+      RemoveRow ->
+          Array2D.deleteRow (Array2D.rows model.colors - 1) model.colors
+              |> asColorsIn model
+
+      RemoveColumn ->
+          Array2D.deleteColumn (Array2D.columns model.colors - 1) model.colors
+              |> asColorsIn model
+
 defaultColor : Color
 defaultColor =
     Css.hex "000000"
@@ -83,11 +93,14 @@ defaultColor =
 
 view : Model -> Html Msg
 view model =
-    let elements = [ selectColorButtonsGrid model
-                   , rowText model
+    let elements = [ rowText model
                    , addRowButton
+                   , removeRowButton
+                   , br [] []
                    , columnText model
                    , addColumnButton
+                   , removeColumnButton
+                   , selectColorButtonsGrid model
                    , colorGrid model
                    , cssLink "normalize.css"
                    , cssLink "skeleton.css"
@@ -160,6 +173,10 @@ addRowButton : Html Msg
 addRowButton =
     button [ onClick AddRow ] [ text "Add row" ]
 
+removeRowButton : Html Msg
+removeRowButton =
+    button [ onClick RemoveRow ] [ text "Remove row" ]
+
 columnText : Model -> Html Msg
 columnText model =
     model.colors
@@ -170,6 +187,10 @@ columnText model =
 addColumnButton : Html Msg
 addColumnButton =
     button [ onClick AddColumn ] [ text "Add column" ]
+
+removeColumnButton : Html Msg
+removeColumnButton =
+    button [ onClick RemoveColumn ] [ text "Remove column" ]
 
 cssLink : String -> Html Msg
 cssLink filename =
